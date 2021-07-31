53a0209ec89ce315176d4d4d71acc24710626a97
Michael <73088874+TheDinosaurCoder@users.noreply.github.com>
2021-07-30 21:37:11 -0400

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+function welcomePerson(person) {
+    console.log("Hey " + person.firstName + " " + person.lastName);
+    return "Hey " + person.firstName + " " + person.lastName;
+}
+var james = {
+    firstName: "James",
+    lastName: "Quack"
+};
+welcomePerson(james);
+for (var i = 0; i < 1000; i++) {
+    console.log(i, "Hello World!");
+}
---
+++ app.ts
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+function welcomePerson(person:Person):string { //need the person obj thats a string
+    console.log(`Hey ${person.firstName} ${person.lastName}`)
+    return `Hey ${person.firstName} ${person.lastName}`;
+}
+
+const james = {
+    firstName: "James",
+    lastName: "Quack"
+}
+
+welcomePerson(james)
+
+interface Person {
+    firstName: string, //properties
+    lastName: string
+}
+
+for (let i = 0; i < 1000; i++){
+    console.log(i, "Hello World!")
+}
